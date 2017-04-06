c300e534c3d64cffa59fd0f3b8f9623e56cb00dd
lquterqtd <378203086@qq.com>
2017-04-06 20:01:42 +0800

    补充提交错误！

---
+++ src/main/java/com/conton/impress/web/controller/LoginParam.java
@@ -0,0 +1,34 @@
+package com.conton.impress.web.controller;
+
+/**
+ * Created by Administrator on 2017-04-06.
+ */
+public class LoginParam {
+    String cellphone;
+    String passWord;
+    String code;
+
+    public String getCellphone() {
+        return cellphone;
+    }
+
+    public void setCellphone(String cellphone) {
+        this.cellphone = cellphone;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
