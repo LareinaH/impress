e697aade9974e4c90d13dc4f3238b5f65bef30ef
lquterqtd <378203086@qq.com>
2017-04-11 15:36:59 +0800

    xxx、

--- src/main/java/com/conton/impress/model/Message.java
+++ src/main/java/com/conton/impress/model/Message.java
@@ -3,7 +3,7 @@ package com.conton.impress.model;
 import com.conton.base.model.BaseModel;
 import java.util.Date;
 import javax.persistence.*;
-
+@Table(name = "message")
 public class Message extends BaseModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
